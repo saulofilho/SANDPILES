5300f8cf4a86e20e86f47ca3df119dc131cdf52e
SauloFilho <saulosilvaf@gmail.com>
2019-05-11 13:06:56 -0300

    #1
    updating

---
+++ notUse.js
@@ -0,0 +1,90 @@
+let defaultColor = [255, 50, 255];
+let colors = [
+  [0],
+  [0, 185, 63], //green
+  [0, 104, 255], //blue
+  [255, 255, 0] //yellow
+];
+
+let sandpiles;
+let nextpiles;
+
+function setup() {
+  createCanvas(600, 600);
+  pixelDensity(1);
+
+//  slider1 = createSlider(0, 10, 4, 1);
+//  var s1 = slider1.value();
+
+  sandpiles = new Array(width).fill().map(i => new Array(height).fill(0));
+  nextpiles = new Array(width).fill().map(i => new Array(height).fill(0));
+
+  sandpiles[width/2][height/2] = 999999999999;
+
+  background(defaultColor[0], defaultColor[1], defaultColor[2]);
+  
+}
+
+function topple() {
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      nextpiles[x][y] = sandpiles[x][y];
+    }
+  }
+
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let num = sandpiles[x][y];
+      if (num >= 4) {
+        nextpiles[x][y] -= 4;
+        if (x+1 < width)
+          nextpiles[x+1][y]++;
+        if (x-1 >= 0)
+          nextpiles[x-1][y]++;
+        if (y+1 < height)
+          nextpiles[x][y+1]++;
+        if (y-1 >= 0)
+          nextpiles[x][y-1]++;
+      }
+    }
+  }
+
+  let tmp = sandpiles;
+  sandpiles = nextpiles;
+  nextpiles = tmp;
+}
+
+function render() {
+  loadPixels();
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let num = sandpiles[x][y];
+      let col = defaultColor;
+      if (num == 0) {
+        col = colors[0];
+      } else if (num == 1) {
+        col = colors[1];
+      } else if (num == 2) {
+        col = colors[2];
+      } else if (num == 3) {
+        col = colors[3];
+      }
+
+      let pix = (x + y * width) * 4;
+      pixels[pix    ] = col[0];
+      pixels[pix + 1] = col[1];
+      pixels[pix + 2] = col[2];
+      // pixels[pix + 3] = 255;
+    }
+  }
+
+  updatePixels();
+}
+
+function draw() {
+  render();
+
+  for (let i = 0; i < 50; i++) {
+    topple();
+  }
+}
---
+++ script.js
@@ -0,0 +1,103 @@
+'use strict';
+
+document.getElementById('coefLoucura').addEventListener("keyup", function() {
+    var coeficienteDeLoucura = parseInt(document.getElementsByClassName('input')[0].value);
+    init(coeficienteDeLoucura);
+  });
+
+  document.getElementById('objForm').addEventListener("keyup", function() {
+    var objectForm = parseInt(document.getElementById('objForm')[0].value);
+    update(objectForm);
+  });
+
+
+var WIDTH = 101,
+    HEIGHT = 101;
+// const PALETTE = ['#f7f1e3', '#706fd3', '#474787', '#40407a', '#2c2c54'] // branco, roxo
+// const PALETTE = ['#f7f1e3', '#33d9b2', '#34ace0', '#706fd3', '#ff5252'] // branco, verde, roxo, vermelho
+// const PALETTE = ['#f7f1e3', '#34e7e4', '#4bcffa', '#575fcf', '#0be881'] // branco, azul, roxo
+// const PALETTE = ['#f7f1e3', '#f6e58d', '#ffbe76', '#ff7979', '#eb4d4b'] // branco, laranja, rosa, amarelo
+// const PALETTE = ['#95afc0', '#7ed6df', '#e056fd', '#686de0', '#30336b'] // cinza, azul, roxo, azul escuro 
+// const PALETTE = ['#dfe6e9', '#55efc4', '#81ecec', '#74b9ff', '#a29bfe'] // branco, verde, azul
+// const PALETTE = ['#f7f1e3', '#686de0', '#f0932b', '#ffbe76', '#e056fd'] // branco, laranja, roxo
+// const PALETTE = ['#f7f1e3', '#ffda79', '#ffb142', '#ff793f', '#ff5252'] // branco, laranja, amarelo
+// const PALETTE = ['#fff', '#ccc', '#999', '#666', '#333'] // pb
+var PALETTE = ['#000', '#66ccff', '#ff99ff', '#66ff66', '#ffff66']; // 
+
+var NEXTFRAME = void 0;
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var grid = [];
+var tempgrid = [];
+
+function init(coeficienteDeLoucura = 0) {
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+
+    // Init the grid with 0's
+    grid = new Array(HEIGHT).fill(0).map(function (x) {
+        return new Array(WIDTH).fill(coeficienteDeLoucura); //0 to 6: efeito do desenho
+    });
+    
+    // Put a lot of sand grains in the middle
+    grid[Math.floor(WIDTH / 2)][Math.floor(HEIGHT / 2)] = Infinity;
+
+    tempgrid = new Array(HEIGHT).fill(0).map(function (x) {
+        return new Array(WIDTH).fill(0);
+    });
+}
+
+function update(objectForm = 0) {
+    // Copy the grid on the tempgrid
+    for (var y = 0; y < HEIGHT; y++) {
+        for (var x = 0; x < WIDTH; x++) {
+            tempgrid[y][x] = grid[y][x];
+        }
+    }
+
+    var n = void 0;
+
+    // Topple the grid
+    for (var _y = 1; _y < HEIGHT - 1; _y++) {
+        for (var _x = 1; _x < WIDTH - 1; _x++) {
+            n = tempgrid[_y][_x];
+            if (n > objectForm) { //-3 to 6: formato do objeto
+                grid[_y][_x] -= 4; //crescimento
+                grid[_y - 1][_x] += 1; //top
+                grid[_y + 1][_x] += 1; //bottom
+                grid[_y][_x - 1] += 1; //left
+                grid[_y][_x + 1] += 1; //right
+            }
+        }
+    }
+}
+
+function draw() {
+    var n = void 0;
+
+    for (var y = 1; y < HEIGHT - 1; y++) {
+        for (var x = 1; x < WIDTH - 1; x++) {
+            n = grid[y][x];
+            ctx.fillStyle = PALETTE[n > 4 ? 4 : n];
+            ctx.fillRect(x, y, 1, 1);
+        }
+    }
+
+    NEXTFRAME = requestAnimationFrame(nextframe);
+}
+
+function nextframe() {
+    update();
+    draw();
+}
+
+init();
+nextframe();
+
+$(document).ready(function() {
+    $("button").click(function() {
+      $("span").toggle();
+    })
+  })
